52dadad6b34625b6d1b58453c17567d22b9a45d1
Daril GJ <darilg44@gmail.com>
2025-08-13 16:42:36 -0600

    CRUD de peliculas

--- app/controllers/pelicula.controller.js
+++ app/controllers/pelicula.controller.js
@@ -30,3 +30,78 @@ exports.create = (req, res) => {
             });
         });
 };
+exports.findAll = (req, res) => {
+    const nombre = req.query.nombre;
+    var condition = nombre ? { nombre: { [Op.like]: `%${nombre}%` } } : null;
+    Pelicula.findAll({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving peliculas."
+            });
+        });
+};
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+    Pelicula.findByPk(id)
+        .then(data => {
+            if (data) {
+                res.send(data);
+            } else {
+                res.status(404).send({
+                    message: `Cannot find Pelicula with id=${id}.`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving Pelicula with id=" + id
+            });
+        });
+};
+exports.update = (req, res) => {
+    const id = req.params.id;
+    Pelicula.update(req.body, {
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Pelicula was updated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Pelicula with id=${id}. Maybe Pelicula was not found or req.body is empty!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Pelicula with id=" + id
+            });
+        });
+};
+exports.delete = (req, res) => {
+    const id = req.params.id;
+    Pelicula.destroy({
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Pelicula was deleted successfully!"
+                });
+            } else {
+                res.send({
+                    message: `Cannot delete Pelicula with id=${id}. Maybe Pelicula was not found!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Pelicula with id=" + id
+            });
+        });
+};
--- app/routes/pelicula.route.js
+++ app/routes/pelicula.route.js
@@ -3,5 +3,10 @@ module.exports = (app) => {
     var router = require('express').Router();
 
     router.post('/', pelicula.create);
+    router.get('/', pelicula.findAll);
+    router.get('/:id', pelicula.findOne);
+    router.put('/:id', pelicula.update);
+    router.delete('/:id', pelicula.delete);
+
     app.use('/api/peliculas', router)
 }
